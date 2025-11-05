4679c6a4b26e6a69f58184e0a2098d332f7bf0dc
ryan <hakimiryan423@gmail.com>
2025-11-04 22:31:32 -0800

    Implemented features to pull metadata from an image

--- CSUN/WeeklyAsmt8/main.py
+++ CSUN/WeeklyAsmt8/main.py
@@ -1,1 +1,18 @@
+import argparse
+import ffmpeg
+import json
 
+
+parser = argparse.ArgumentParser()
+parser.add_argument("image", help = "image filepath")
+args = parser.parse_args()
+
+print ("image path: ", args.image)
+info = ffmpeg.probe(args.image)
+print(type(info), info.keys())
+print(json.dumps(info, indent=2))
+
+out_path = "metadata.txt"
+with open(out_path, "w", encoding="utf-8") as file:
+    json.dump(info, file, indent=2)
+print("Saved metadata to:", out_path)
