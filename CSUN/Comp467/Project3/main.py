7fae4e78a65a38693c00d3e8fba3460ee550cf43
ryan <hakimiryan423@gmail.com>
2025-11-06 20:49:20 -0800

    Project 3 Update
    - set up argparse with basic inputs like filepath and owner
    
    - implemented a function to extract the path, stem, and suffix to create a new file name for the result file (note: result feature not implemented yet, file is unchanged)

--- CSUN/Comp467/Project3/main.py
+++ CSUN/Comp467/Project3/main.py
@@ -1,1 +1,26 @@
+### project 3 vFxML ###
+# get it? cuz FML means fuck my life, and its kinda in the project name? whatever...
+
+#imports
+import argparse
+from pathlib import Path
+
+#functions
+def versionName(file, owner):
+    path = Path(file)
+    print("Stem:", path.stem)
+    print("Suffix:", path.suffix)
+    print(f"Base:{path.stem}_VFX_{owner}_v01{path.suffix}")
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("file", help = "enter file name or path")
+parser.add_argument("--owner", help = "define the owner")
+
+args = parser.parse_args()
+
+print("File:", args.file)
+print("Owner:", args.owner)
+
+versionName(args.file, args.owner)
 
