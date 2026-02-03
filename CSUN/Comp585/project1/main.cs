3ac3053fc0a8cd7c9cc3d1371045219d3615e831
ryan <hakimiryan423@gmail.com>
2026-02-03 15:00:16 -0800

    Comp 585 Project 1 Update
    - All functions declared
    - Functions using simple arithmetic implemented
    - Functions using Math.example declared but not implemented
    - Switch case implemented

--- CSUN/Comp585/project1/main.cs
+++ CSUN/Comp585/project1/main.cs
@@ -1,10 +1,7 @@
 using System;
 using System.IO;
 
-// global vars
 int selection;
-int num1;
-int num2;
 
 static void printInterface()
 {
@@ -17,18 +14,94 @@ static void printInterface()
         |   2. Subtract Operation                |\n
         |   3. Multiply Operation                |\n
         |   4. Divide Operation                  |\n
-        |   5. Exit                              |\n
+        |   5. Modulus                           |\n
+        |   6. Square Root                       |\n
+        |   7. Square                            |\n
+        |   8. Reciprocal                        |\n
+        |   9. Exit                              |\n
         |========================================|\n"
     );
 
     // read user selection
     Console.WriteLine("\nSelect an operation: ");
     int selection = Console.ReadLine();
+};
 
-    // read user nums
-    Console.WriteLine("\n\nEnter the first number: ");
-    int num1 = Console.ReadLine();
-    Console.WriteLine("\n\nEnter the second number: ");
-    int num2 = Console.ReadLine();
-}
+static void guideUser(int selection)
+{
+    switch(selection)
+    {
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+            Console.WriteLine("\nEnter the first number: ");
+            double num1 = Console.ReadLine();
+            Console.WriteLine("\nEnter the second number: ");
+            double num2 = Console.ReadLine();
+            break;
+        case 6:
+        case 7:
+        case 8:
+            Console.WriteLine("\nEnter a number: ");
+            double num = Console.ReadLine();
+        case 9:
+            exit();
+    };
+};
 
+// selection 1
+double add(double num1, double num2)
+{
+    return num1 + num2;
+};
+
+// selection 2
+double subract(double num1, double num2)
+{
+    return num1 - num2;
+};
+
+// selection 3
+double multiply(double num1, double num2)
+{
+    return num1 * num2;
+};
+
+// selection 4
+double divide(double num1, double num2)
+{
+    return num1 / num2;
+};
+
+// selection 5
+double modulus(double num1, double num2)
+{
+    return num1 % num2;
+};
+
+// selection 6
+double squareRoot(double num)
+{
+    return;
+};
+
+// selection 7
+double square(double num)
+{
+    return;
+};
+
+// selection 8
+double reciprocal(double num)
+{
+    return;
+};
+
+// selection 9
+static void exit()
+{
+    Console.WriteLine("\n\nExiting now...");
+    Environment.Exit(0);
+};
