6a1b16b6dd709b5589fb31ced8bd43e63dcc6e2a
ryan <hakimiryan423@gmail.com>
2026-02-03 16:19:58 -0800

    Comp 585 Project 1 (Part 1) Complete
    - All functions implemented
    - All test cases passed
    - Ready for Part 1 submission

--- CSUN/Comp585/project1/main.cs
+++ CSUN/Comp585/project1/main.cs
@@ -1,107 +1,138 @@
 using System;
-using System.IO;
 
-int selection;
-
-static void printInterface()
+class CalculatorApp
 {
-    // print menu
-    Console.WriteLine(
-        @"\n|========================================|\n
-        |       Welcome to Basic Calculator       |\n
-        |========================================|\n
-        |   1. Add Operation                     |\n
-        |   2. Subtract Operation                |\n
-        |   3. Multiply Operation                |\n
-        |   4. Divide Operation                  |\n
-        |   5. Modulus                           |\n
-        |   6. Square Root                       |\n
-        |   7. Square                            |\n
-        |   8. Reciprocal                        |\n
-        |   9. Exit                              |\n
-        |========================================|\n"
-    );
-
-    // read user selection
-    Console.WriteLine("\nSelect an operation: ");
-    int selection = Console.ReadLine();
-};
-
-static void guideUser(int selection)
-{
-    switch(selection)
+    static void Main()
     {
-        case 1:
-        case 2:
-        case 3:
-        case 4:
-        case 5:
-            Console.WriteLine("\nEnter the first number: ");
-            double num1 = Console.ReadLine();
-            Console.WriteLine("\nEnter the second number: ");
-            double num2 = Console.ReadLine();
+        PrintInterface();
+    }
+    
+    static void PrintInterface()
+    {
+        // print menu
+        Console.WriteLine();
+        Console.WriteLine("""
+
+        |========================================|
+        |         Welcome the Calculator         |
+        |========================================|
+        |   1. Add Operation                     |
+        |   2. Subtract Operation                |
+        |   3. Multiply Operation                |
+        |   4. Divide Operation                  |
+        |   5. Modulus                           |
+        |   6. Square Root                       |
+        |   7. Square                            |
+        |   8. Reciprocal                        |
+        |   9. Exit                              |
+        |========================================|
+
+        """);
+
+        // read user selection
+        Console.WriteLine("Select an operation:");
+        if (!int.TryParse(Console.ReadLine(), out int selection)) return;
+
+        // call guide func
+        GuideUser(selection);
+    }
+
+    static void GuideUser(int selection)
+    {
+        double result = 0;
+        switch (selection)
+        {
+            default:
+            Console.WriteLine("\nInvalid selection.");
+            PrintInterface();
             break;
-        case 6:
-        case 7:
-        case 8:
-            Console.WriteLine("\nEnter a number: ");
-            double num = Console.ReadLine();
-        case 9:
-            exit();
-    };
-};
 
-// selection 1
-double add(double num1, double num2)
-{
-    return num1 + num2;
-};
+            case >= 1 and <= 5:
+                Console.WriteLine("\nEnter the first number:");
+                if (!double.TryParse(Console.ReadLine(), out double num1)) return;
+                Console.WriteLine("\nEnter the second number:");
+                if (!double.TryParse(Console.ReadLine(), out double num2)) return;
+                
+                if (selection == 1) result = Add(num1, num2);
+                else if (selection == 2) result = Subtract(num1, num2);
+                else if (selection == 3) result = Multiply(num1, num2);
+                else if (selection == 4) result = Divide(num1, num2);
+                else if (selection == 5) result = Modulus(num1, num2);
+                Console.WriteLine($"\nResult:\n{result}");
+                PrintInterface();
+                break;
 
-// selection 2
-double subract(double num1, double num2)
-{
-    return num1 - num2;
-};
+            case 6:
+            case 7:
+            case 8:
+                Console.WriteLine("\nEnter a number:");
+                if (!double.TryParse(Console.ReadLine(), out double num)) return;
 
-// selection 3
-double multiply(double num1, double num2)
-{
-    return num1 * num2;
-};
+                if (selection == 6) result = SquareRoot(num);
+                else if (selection == 7) result = Square(num);
+                else if (selection == 8) result = Reciprocal(num);
+                Console.WriteLine($"\nResult:\n{result}");
+                PrintInterface();
+                break;
 
-// selection 4
-double divide(double num1, double num2)
-{
-    return num1 / num2;
-};
+            case 9:
+                Exit();
+                break;
+        }
+    }
 
-// selection 5
-double modulus(double num1, double num2)
-{
-    return num1 % num2;
-};
+    // selection 1
+    static double Add(double num1, double num2)
+    {
+        return num1 + num2;
+    }
 
-// selection 6
-double squareRoot(double num)
-{
-    return;
-};
+    // selection 2
+    static double Subtract(double num1, double num2)
+    {
+        return num1 - num2;
+    }
 
-// selection 7
-double square(double num)
-{
-    return;
-};
+    // selection 3
+    static double Multiply(double num1, double num2)
+    {
+        return num1 * num2;
+    }
 
-// selection 8
-double reciprocal(double num)
-{
-    return;
-};
+    // selection 4
+    static double Divide(double num1, double num2)
+    {
+        return num1 / num2;
+    }
 
-// selection 9
-static void exit()
-{
-    Console.WriteLine("\n\nExiting now...");
-    Environment.Exit(0);
-};
+    // selection 5
+    static double Modulus(double num1, double num2)
+    {
+        return num1 % num2;
+    }
+
+    // selection 6
+    static double SquareRoot(double num)
+    {
+        return Math.Sqrt(num);
+    }
+
+    // selection 7
+    static double Square(double num)
+    {
+        return num * num;
+    }
+
+    // selection 8
+    static double Reciprocal(double num)
+    {
+        return 1 / num;
+    }
+
+    // selection 9
+    static void Exit()
+    {
+        Console.WriteLine("\nExiting now...\n\n");
+        Environment.Exit(0);
+    }
+}
